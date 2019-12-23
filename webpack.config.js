9d73064e44c0cc0ee9d64558f9b703dda6502d2f
Mao39 <steallermail@gmail.com>
2019-12-23 18:37:56 +0200

    fix cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
@@ -26,7 +26,7 @@ module.exports = {
     ]
   },
   plugins: [ 
-    new CleanWebpackPlugin('dist', {} ),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'style.[contenthash].css',
     }),
