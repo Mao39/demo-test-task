04052aae0ece0645b120d9aad0dc217b4f398b6b
Mao39 <steallermail@gmail.com>
2019-12-24 11:52:04 +0200

    change sass to scss and reconfig scss

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const isDevMode = 'production' !== process.env.NODE_ENV;
 
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[hash].js',
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -26,15 +27,42 @@ module.exports = {
         },
       },
       {
-        test: /\.sass$/,
-        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        test: /\.scss$/,
+        use: [ isDevMode ? "style-loader" : MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
+          }
+        }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: './postcss.config.js'
+              }
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }],
       },
     ],
   },
-  plugins: [
+  plugins: isDevMode ? [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new WebpackMd5Hash(),
+  ] : [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css',
+      filename: '[name].[hash].css',
     }),
     new HtmlWebpackPlugin({
       inject: false,
